a274b27eb9ce07b9753f187794d0781ea949027f
yaronm <yaron.miro@gmail.com>
2019-07-22 12:56:15 +0300

    Adding node.js server with PM2

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT;
 
 app.get('/', (req, res) => res.send(`${process.env.NODE_ENV} enviroment says: Hello World!`));
 app.listen(port, () => {console.log(`Example app listening on port ${port}!`)});
