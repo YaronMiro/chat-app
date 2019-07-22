74873194c373058cdb9b6ec15f18a80f2d99db0d
yaronm <yaron.miro@gmail.com>
2019-07-22 15:15:46 +0300

    Adding redis

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Chat-app
-A Real time chat application (Docker | React | Node.js | Socket.io | Redis)
+A Real time chat application (Docker | React | Express | Socket.io | Redis)
 
 
 ### Dependencies
@@ -10,5 +10,11 @@ A Real time chat application (Docker | React | Node.js | Socket.io | Redis)
 1.  Run the following commands -> `docker-compose up`
 
 
-### Docker enviroment variables
-Server -> The variables are defined in the `server.env` file.
+### Docker compose enviroment variables
+The variables are defined in the `.env` file.
+
+
+### Docker containers enviroment variables
+* __server__ -> The variables are defined in the `server-container.env` file.
+* __redis__ -> The variables are defined in the `redis-container.env` file.
+* __client__ -> The variables are defined in the `client-container.env` file.
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
-const port = process.env.SERVER_PORT;
+const PORT = process.env.PORT || 3000;
+const ENVIRONMENT_MODE = process.env.NODE_ENV || 'development';
 
-app.get('/', (req, res) => res.send(`${process.env.NODE_ENV} enviroment says: Hello World!`));
-app.listen(port, () => {console.log(`Example app listening on port ${port}!`)});
+const redisClient = require('./services/redis/redis');
+
+
+app.get('/', (req, res) => res.send(`${ENVIRONMENT_MODE} enviroment says: Hello World!`));
+
+// https://github.com/luin/ioredis
+
+// https://codewithhugo.com/setting-up-express-and-redis-with-docker-compose/
+
+// http://localhost:3000/store/my-key\?some\=value\&some-other\=other-value Success
+app.get('/store/:key', async (req, res) => {
+    const { key } = req.params;
+    const value = req.query;
+    await redisClient.setAsync(key, JSON.stringify(value));
+    return res.send('Success');
+});
+
+// http://localhost:3000/my-key
+app.get('/:key', async (req, res) => {
+    const { key } = req.params;
+    const rawData = await redisClient.getAsync(key);
+    return res.json(JSON.parse(rawData));
+});
+
+
+app.listen(PORT, () => {console.log(`Example app listening on port ${PORT}!`)});
